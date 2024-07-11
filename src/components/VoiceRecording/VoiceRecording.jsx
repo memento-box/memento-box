b21eccc2273150b45bf7134c2f5b9a0d72313691
David Smith <djsmith611@me.com>
2024-07-11 15:25:39 -0700

    added outline to VoiceRecording component

--- src/components/VoiceRecording/VoiceRecording.jsx
+++ src/components/VoiceRecording/VoiceRecording.jsx
@@ -1,11 +1,41 @@
-const Voice_Recording = () => {
+import { Typography } from "@mui/material";
+import { useState } from "react";
 
-    return (
-        <>
-        Voice Recording Goes Here!
-        </>
-    )
+export default function VoiceRecording() {
+  const [notes, setNotes] = useState([]); // State for notes to be rendered
 
+  const fetchNotes = () => {
+    // API call to retrieve relevant voice notes
+    setNotes([]);
+  };
+
+  useEffect(() => {
+    // Fetch voice notes on component load
+    fetchNotes();
+  }, []);
+
+  return (
+    <div>
+      <div className="notes-actions">
+        <button>Upload</button>
+        {/* button will pull up small upload form */}
+        <button>Record</button>
+        {/* button will use recording package and post to Cloudinary */}
+      </div>
+      <div className="notes-display">
+        {notes.length === 0 ? (
+          <Typography> Added voice notes will display here! </Typography>
+        ) : (
+          notes.map((note) => 
+            <div>
+                {/* add headphone icon */}
+                <Typography>{note.fileName}</Typography> {/* title */}
+                {/* add play/pause */}
+                {/* add visualization here */}
+                {/* add length here */}
+            </div>)
+        )}
+      </div>
+    </div>
+  );
 }
-
-export default Voice_Recording;
