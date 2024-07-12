713f52d0cecb6406b7b98e82a1c13b5834012270
Sarah McCartney <smmc16@gmail.com>
2024-07-11 20:40:01 -0500

    Created thanks.router.js and added it to the server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route Includes
 const userRouter = require('./routes/user.router');
 const userInfoRouter = require('./routes/userInfo.router');
+const thanksRouter = require('./routes/thanks.router');
 
 // Express Middleware
 app.use(express.json());
@@ -26,6 +27,7 @@ app.use(passport.session());
 // Routes
 app.use('/api/user', userRouter);
 app.use('/api/userInfo', userInfoRouter);
+app.use('/api/thanks', thanksRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
