70c247b1fd470b06bf4aff78b3b7c5ec2f032046
Kenneth Grina <grina.kenneth@gmail.com>
2024-07-13 12:24:07 -0500

    working on hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,16 +2,16 @@
 function Hero() {
 
     return(
-        <div>
-            <div>
-                <h1>Digital gifts, done differently</h1>
+        <div id="hero">
+            <div id="text">
+                <h1>Digital gifts, <br /> done differently</h1>
                 <h6>Introducing the Memento Box</h6>
                 <button>Learn more</button>
             </div>
-            <div id="img-container">
+            
                 <img src="/boxes/white-red-ribbon.png" id="closed-box"/>
                 <img src="/RecipientBoxOpen.png" id="hover-box"/>
-            </div>
+         
 
         </div>
     )
