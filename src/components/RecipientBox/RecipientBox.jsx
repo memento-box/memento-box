7763e415c8b26dc93bb715770494d59ff0285d42
Sean Harrison <sharrison9@gmail.com>
2024-07-13 19:42:47 -0500

    Add navigation links from RecipientBox to respective pages and set up routes

--- src/components/RecipientBox/RecipientBox.jsx
+++ src/components/RecipientBox/RecipientBox.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import {useSelector} from 'react-redux';
+// src/components/RecipientBox/RecipientBox.jsx
+
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './RecipientBox.css';
 
 function RecipientBox() {
@@ -7,38 +9,38 @@ function RecipientBox() {
     let [greeting, setGreeting] = useState('Happy Birthday');
     let [collabs, setCollabs] = useState(['Lons', 'Sarah', 'Sean', 'Zoe']);
 
-    useEffect(() => {
+    const history = useHistory();
 
-    }, [])
+    const navigateTo = (path) => {
+        history.push(path);
+    };
 
-
-  return (
-    <div id='grid'>
-        <div id="collaborators">
-        <h3 id='greeting'>{greeting}</h3>
-        <h6 id='from'>From:</h6>
-        {collabs.map((person) => 
-            <p className='person' key={person}>{person}</p>
-        )}
-        </div>
-        <img src='/RecipientBoxOpen.png' id='boxOpen' useMap='#image-map'/>
+    return (
+        <div id='grid'>
+            <div id="collaborators">
+                <h3 id='greeting'>{greeting}</h3>
+                <h6 id='from'>From:</h6>
+                {collabs.map((person) => 
+                    <p className='person' key={person}>{person}</p>
+                )}
+            </div>
+            <img src='/RecipientBoxOpen.png' id='boxOpen' useMap='#image-map'/>
             <map name="image-map">
-                <area target="" alt="Photos" title="Photos" href="" coords="89,71,249,303" shape="rect" />
-                <area target="" alt="Videos" title="Videos" href="" coords="254,78,529,269" shape="rect" />
-                <area target="" alt="Voice notes" title="Voice notes" href="" coords="270,352,540,274" shape="rect" />
-                <area target="" alt="Gifts" title="Gifts" href="" coords="92,310,245,417" shape="rect" />
-                <area target="" alt="Mixtape" title="Mixtape" href="" coords="80,422,262,537" shape="rect" />
-                <area target="" alt="Letters" title="Letters" href="" coords="266,357,538,535" shape="rect" />
+                <area target="" alt="Photos" title="Photos" onClick={() => navigateTo('/recipient/photos')} coords="89,71,249,303" shape="rect" />
+                <area target="" alt="Videos" title="Videos" onClick={() => navigateTo('/recipient/videos')} coords="254,78,529,269" shape="rect" />
+                <area target="" alt="Voice notes" title="Voice notes" onClick={() => navigateTo('/recipient/voicenotes')} coords="270,352,540,274" shape="rect" />
+                <area target="" alt="Gifts" title="Gifts" onClick={() => navigateTo('/recipient/gifts')} coords="92,310,245,417" shape="rect" />
+                <area target="" alt="Mixtape" title="Mixtape" onClick={() => navigateTo('/recipient/mixtape')} coords="80,422,262,537" shape="rect" />
+                <area target="" alt="Letters" title="Letters" onClick={() => navigateTo('/recipient/letters')} coords="266,357,538,535" shape="rect" />
             </map>
-      
-        <img 
-            src='/RecipientBoxClosed.png' 
-            id='boxClosed' 
-            onClick={() => setOpenBox(true)}
-            className={openBox ? 'animation' : ''}
+            <img 
+                src='/RecipientBoxClosed.png' 
+                id='boxClosed' 
+                onClick={() => setOpenBox(true)}
+                className={openBox ? 'animation' : ''}
             />
-    </div>
-  );
+        </div>
+    );
 }
 
 export default RecipientBox;
