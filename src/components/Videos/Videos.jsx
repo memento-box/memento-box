553d7d148fb00d49811f45e78f8aa0d227b53cf2
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-07-14 18:01:06 -0500

    Added api url for cloudinary library, videos will upload and then console log a media url.

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -3,15 +3,29 @@ import { useState } from "react";
 
 const Videos = () => {
 
-    const cloudName = import.meta.env.CLOUDINARY_CLOUD_NAME;
-    const upPreset = import.meta.env.UPLOAD_PRESET;
+    const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+    const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+
 
     const [fileUpload, setFileUpload] = useState([]);
 
 
 
     const videoUpload = () => {
-        console.log(fileUpload);
+      console.log(fileUpload);
+
+      const formData = new FormData();
+      formData.append('file', fileUpload);
+      formData.append('upload_preset', uploadPreset);
+      let apiUrl = `https://api.cloudinary.com/v1_1/${cloudName}/video/upload`;
+
+      axios.post(apiUrl, formData)
+      .then((r) => {
+        console.log(r.data.url);
+      })
+      .catch((e) => {
+        console.log("Something went wrong with your video upload", e)
+      })
 
     }
 
