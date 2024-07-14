e7ee46c3569b99f56dcbc335d5bc2230a6391a04
Sean Harrison <sharrison9@gmail.com>
2024-07-14 01:13:12 -0500

    Improve letters popup: fetch and display content from text files, align UI

--- src/components/RecipientBox/RecipientBox.jsx
+++ src/components/RecipientBox/RecipientBox.jsx
@@ -8,68 +8,67 @@ import RecipientMixtape from '../RecipientMixtape/RecipientMixtape';
 import RecipientLetters from '../RecipientLetters/RecipientLetters';
 import './RecipientBox.css';
 
-Modal.setAppElement('#react-root'); // Ensure this matches your root element ID
+Modal.setAppElement('#react-root');
 
 function RecipientBox() {
-    const [openBox, setOpenBox] = useState(false);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [modalContent, setModalContent] = useState(null);
-    const [greeting, setGreeting] = useState('Happy Birthday');
-    const [collabs, setCollabs] = useState(['Lons', 'Sarah', 'Sean', 'Zoe']);
+  const [openBox, setOpenBox] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [greeting, setGreeting] = useState('Happy Birthday');
+  const [collabs, setCollabs] = useState(['Lons', 'Sarah', 'Sean', 'Zoe']);
 
-    const openModal = (content) => {
-        setModalContent(content);
-        setModalIsOpen(true);
-    };
+  const openModal = (content) => {
+    setModalContent(content);
+    setModalIsOpen(true);
+  };
 
-    const closeModal = () => {
-        setModalIsOpen(false);
-        setModalContent(null);
-    };
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setModalContent(null);
+  };
 
-    const handleAreaClick = (e, content) => {
-        e.preventDefault(); // Prevent default action
-        openModal(content);
-    };
+  useEffect(() => {
+    // Any additional setup can go here
+  }, []);
 
-    useEffect(() => {
-        // Any additional setup can go here
-    }, []);
+  const handleImageMapClick = (event, content) => {
+    event.preventDefault();
+    openModal(content);
+  };
 
-    return (
-        <div id='grid'>
-            <div id="collaborators">
-                <h3 id='greeting'>{greeting}</h3>
-                <h6 id='from'>From:</h6>
-                {collabs.map((person) => 
-                    <p className='person' key={person}>{person}</p>
-                )}
-            </div>
-            <img src='/RecipientBoxOpen.png' id='boxOpen' useMap='#image-map'/>
-            <map name="image-map">
-                <area alt="Photos" title="Photos" onClick={(e) => handleAreaClick(e, <RecipientPhotos />)} coords="89,71,249,303" shape="rect" />
-                <area alt="Videos" title="Videos" onClick={(e) => handleAreaClick(e, <RecipientVideos />)} coords="254,78,529,269" shape="rect" />
-                <area alt="Voice notes" title="Voice notes" onClick={(e) => handleAreaClick(e, <RecipientVoiceNotes />)} coords="270,352,540,274" shape="rect" />
-                <area alt="Gifts" title="Gifts" onClick={(e) => handleAreaClick(e, <RecipientGifts />)} coords="92,310,245,417" shape="rect" />
-                <area alt="Mixtape" title="Mixtape" onClick={(e) => handleAreaClick(e, <RecipientMixtape />)} coords="80,422,262,537" shape="rect" />
-                <area alt="Letters" title="Letters" onClick={(e) => handleAreaClick(e, <RecipientLetters />)} coords="266,357,538,535" shape="rect" />
-            </map>
-            <img 
-                src='/RecipientBoxClosed.png' 
-                id='boxClosed' 
-                onClick={() => setOpenBox(true)}
-                className={openBox ? 'animation' : ''}
-            />
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="Recipient Box Modal"
-            >
-                {modalContent}
-                <button onClick={closeModal}>Close</button>
-            </Modal>
-        </div>
-    );
+  return (
+    <div id='grid'>
+      <div id="collaborators">
+        <h3 id='greeting'>{greeting}</h3>
+        <h6 id='from'>From:</h6>
+        {collabs.map((person) => 
+          <p className='person' key={person}>{person}</p>
+        )}
+      </div>
+      <img src='/RecipientBoxOpen.png' id='boxOpen' useMap='#image-map'/>
+      <map name="image-map">
+        <area alt="Photos" title="Photos" href="#" onClick={(event) => handleImageMapClick(event, <RecipientPhotos />)} coords="89,71,249,303" shape="rect" />
+        <area alt="Videos" title="Videos" href="#" onClick={(event) => handleImageMapClick(event, <RecipientVideos />)} coords="254,78,529,269" shape="rect" />
+        <area alt="Voice notes" title="Voice notes" href="#" onClick={(event) => handleImageMapClick(event, <RecipientVoiceNotes />)} coords="270,352,540,274" shape="rect" />
+        <area alt="Gifts" title="Gifts" href="#" onClick={(event) => handleImageMapClick(event, <RecipientGifts />)} coords="92,310,245,417" shape="rect" />
+        <area alt="Mixtape" title="Mixtape" href="#" onClick={(event) => handleImageMapClick(event, <RecipientMixtape />)} coords="80,422,262,537" shape="rect" />
+        <area alt="Letters" title="Letters" href="#" onClick={(event) => handleImageMapClick(event, <RecipientLetters onBack={closeModal} />)} coords="266,357,538,535" shape="rect" />
+      </map>
+      <img 
+        src='/RecipientBoxClosed.png' 
+        id='boxClosed' 
+        onClick={() => setOpenBox(true)}
+        className={openBox ? 'animation' : ''}
+      />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Recipient Box Modal"
+      >
+        {modalContent}
+      </Modal>
+    </div>
+  );
 }
 
 export default RecipientBox;
