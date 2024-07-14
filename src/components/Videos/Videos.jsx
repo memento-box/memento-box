7bbcaf3e12fdb568c4714fe5c174a9c3d19283a2
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-07-14 15:42:00 -0500

    Created basic file upload function for videos page that console logs information about the file when the upload button is clicked.

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,9 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Videos = () => {
 
+    const cloudName = import.meta.env.CLOUDINARY_CLOUD_NAME;
+    const upPreset = import.meta.env.UPLOAD_PRESET;
+
+    const [fileUpload, setFileUpload] = useState([]);
+
+
+
+    const videoUpload = () => {
+        console.log(fileUpload);
+
+    }
+
     return (
-        <>
-        Videos go here!
-        </>
+        <div id="container">
+           Video Upload: 
+
+        <form onSubmit={videoUpload}>
+            <input type='file' accept='video/*' onChange={(e) => setFileUpload(e.target.files[0])}/>
+            <button type='submit'>Upload Video</button>
+        </form>
+
+        </div>
     )
 
 }
