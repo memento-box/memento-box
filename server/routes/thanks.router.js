aece94bb7e02bb1a5a4939dff71c76ff8622820c
Sarah McCartney <smmc16@gmail.com>
2024-07-14 21:35:16 -0500

    Added GET route for thanks messages

--- server/routes/thanks.router.js
+++ server/routes/thanks.router.js
@@ -2,13 +2,23 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/messages/:id', (req, res) => {
+queryText = `SELECT "user"."first_name", "user"."last_name", "box_thanks"."message" FROM "user" 
+JOIN "user_box_thanks" ON "user"."id" = "user_box_thanks"."user_id"
+JOIN "box_thanks" ON "user_box_thanks"."box_thanks_id" = "box_thanks"."id"
+WHERE "box_thanks"."box_id" = $1;`;
+pool
+    .query(queryText, [req.params.id])
+    .then((dbRes) => {res.status(200).send(dbRes.rows)})
+    .catch((err) => {
+      console.log('Fetching thanks messages failed:', err);
+      res.sendStatus(500);
+    });
 });
 
 
-router.post('/', (req, res) => {
-
+router.post('/messages', (req, res) => {
+    
 });
 
 module.exports = router;
