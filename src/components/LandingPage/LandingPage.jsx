c272c17c51b3eb7f585e23ad8724124d1a4d7876
Kenneth Grina <grina.kenneth@gmail.com>
2024-07-14 20:20:07 -0500

    link in footer to contact us form from mailchimp

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 import MementoBoxInfo from '../MementoBoxInfo/MementoBoxInfo';
-import ContactPage from '../ContactPage/ContactPage';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
@@ -11,7 +10,6 @@ function LandingPage() {
   return (
     <div className="container">
       <h2>{heading}</h2>
-      <ContactPage />
       <div className="grid">
         <div className="grid-col grid-col_8">
           <MementoBoxInfo />
