c9354edb28ee0ed8c4b4c3ac22c83e26c5323408
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-07-16 15:37:36 -0500

    Created forms for both the letter image upload and the straight text upload, created a function that will process the form differently depending on what is uploaded

--- src/components/Letters/Letters.jsx
+++ src/components/Letters/Letters.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 const Letters = () => {
+
+    const [fileUpload, setFileUpload] = useState([]);
+    const [fileMap, setFileMap] = useState([]);
+
+    const uploadText = () => {
+        console.log(fileUpload);
+        return <>
+        </>
+    }
 
     return (
         <>
-        Letters Go Here!
+        Either upload or craft a personalized letter to add to the box!
+
+        <form onSubmit={uploadText}>
+           <input type='file' accept='image/*' onChange={(e) => setFileUpload(e.target.files[0])}/> 
+           <button type='submit'>Upload</button>
+           </form>
+
+        <form onSubmit={uploadText}>
+           <input type='text' style={{height:'150px', width: '400px'}} onChange={(e) => setFileUpload(e.target.value)} />
+           <button type='submit'>Submit</button>
+        </form>
         </>
     )
 
