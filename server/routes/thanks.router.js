e8bafc57787e13e9056ea1f0871a830163cd4404
Sarah McCartney <smmc16@gmail.com>
2024-07-16 13:25:19 -0500

    updated database to change recipient_id to thanks_receiver_id and updated the routes

--- server/routes/thanks.router.js
+++ server/routes/thanks.router.js
@@ -4,10 +4,9 @@ const router = express.Router();
 
 router.get('/messages', (req, res) => {
 queryText = `
-SELECT "user"."first_name", "user"."last_name", "box_thanks"."message" FROM "user" 
-JOIN "memento_box" ON "user"."email" = "memento_box"."recipient_email"
+SELECT "memento_box"."recipient_name", "box_thanks"."message" FROM "memento_box" 
 JOIN "box_thanks" ON "box_thanks"."box_id" = "memento_box"."id"
-WHERE "box_thanks"."box_id" = $1 AND "box_thanks"."recipient_id" = $2;
+WHERE "box_thanks"."box_id" = 2 AND "box_thanks"."thanks_receiver_id" = 1;
 `;
 pool
     .query(queryText, [req.body.boxID, req.body.userID])
@@ -21,7 +20,7 @@ pool
 
 router.post('/messages', (req, res) => {
     queryText = `
-      INSERT INTO "box_thanks" ("message", "recipient_id", "box_id") VALUES ($1, $2, $3);
+      INSERT INTO "box_thanks" ("message", "thanks_receiver_id", "box_id") VALUES ($1, $2, $3);
     `;
     pool
         .query(queryText, [req.body.message, req.body.recipientID, req.body.boxID])
