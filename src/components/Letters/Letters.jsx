df4791fbb39374eb3f563888d7a18adba6aa8d9e
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-07-17 14:50:47 -0500

    an uploaded image in the Letters component will now display on the page.

--- src/components/Letters/Letters.jsx
+++ src/components/Letters/Letters.jsx
@@ -1,30 +1,64 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Letters = () => {
 
+    const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME; //VITE needed for import
+    const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+
     const [fileUpload, setFileUpload] = useState([]);
+    const [textUpload, setTextUpload] = useState([]);
     const [fileMap, setFileMap] = useState([]);
 
-    const uploadText = () => {
+    let displaySwitch = false
+
+
+    const uploadFile = () => {
         console.log(fileUpload);
-        return <>
-        </>
+
+        displaySwitch = true;
+
+        const formData = new FormData();
+        formData.append('file', fileUpload);
+        formData.append('upload_preset', uploadPreset);
+        let apiUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`; //appending cloud names & presets to url
+
+        axios.post(apiUrl, formData)
+        .then((r) => { 
+          console.log(r.data.url);
+          setFileMap([r.data.url])
+        })
+         .catch((e) => {
+         console.log("Something went wrong with your letter upload", e)
+        })
+    };
+
+    const uploadText =() => {
+        console.log(textUpload)
+
+        displaySwitch = false;
+        setFileMap(textUpload);
     }
 
     return (
-        <>
-        Either upload or craft a personalized letter to add to the box!
+        <div>
+        <p>Either upload or craft a personalized letter to add to the box!</p>
 
-        <form onSubmit={uploadText}>
+        <form onSubmit={uploadFile}>
            <input type='file' accept='image/*' onChange={(e) => setFileUpload(e.target.files[0])}/> 
            <button type='submit'>Upload</button>
            </form>
 
         <form onSubmit={uploadText}>
-           <input type='text' style={{height:'150px', width: '400px'}} onChange={(e) => setFileUpload(e.target.value)} />
+           <input type='text' style={{height:'150px', width: '400px'}} onChange={(e) => setTextUpload(e.target.value)} />
            <button type='submit'>Submit</button>
         </form>
-        </>
+
+        {
+            fileMap.map((file) => ( <img src={file} style={{height: '480px', width: '720px'}} /> ))
+        }
+
+        </div>
     )
 
 }
