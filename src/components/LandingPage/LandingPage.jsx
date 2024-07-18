ba2f4001da40368a6f4f86e4a4bd27f0f30b3f72
Kenneth Grina <grina.kenneth@gmail.com>
2024-07-17 20:12:25 -0500

    working on learnmore button

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactPlayer from 'react-player';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
@@ -7,11 +7,23 @@ import Hero from '../Hero/Hero'
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
-  const history = useHistory();
-  const url = 'public/videos/memento-Intro.mp4'
+  const url = 'public/videos/memento-Intro.mp4';
+  // const introVideo = useRef(null);
+  // const toVideo = () => {
+  //   //  useRef with .focus in the function to target te video componenet? ref needed in video component.
+  //     let inputElement = introVideo.current;
+  //     inputElement.focus();
+  // };
+
   return (
     <div className="container">
       <h2>{heading}</h2>
+      <div id="text">
+                <h1>Digital gifts, <br /> done differently</h1>
+                <h6>Introducing the Memento Box</h6>
+                <a href="#video-wrapper" >Learn more</a>
+                {/* <button onClick={() => toVideo()}>Learn more</button> */}
+      </div>
       <Hero />
       <div id="video-wrapper">
                 <ReactPlayer url={url} controls={true}/>
