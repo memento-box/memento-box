98144dce45c11160f6c375b8201fd27b0d0a54a9
Sarah McCartney <smmc16@gmail.com>
2024-07-17 19:10:22 -0500

    added form

--- src/components/ThankYouPage/ThankYouPage.jsx
+++ src/components/ThankYouPage/ThankYouPage.jsx
@@ -3,10 +3,38 @@ import {useSelector} from 'react-redux';
 import './ThankYouPage.css';
 
 function ThankYouPage() {
+    let [collabs, setCollabs] = useState(['Lons', 'Sarah', 'Sean', 'Zoe']);
+    let [message, setMessage] = useState();
+    let [receivers, setReceivers] = useState([]);
+    let [selectAll, setSelectAll] = useState(false);
+
+    function handleSubmit(event) {
+        event.preventDefault;
+        console.log(message);
+    }
+
+    if(selectAll) {
+
+    }
 
   return (
-    <div>
-      <h1 id='heading'>Give Thanks!</h1>
+    <div id='pageGrid'>
+        <h1 id='heading'>Give Thanks!</h1>
+        <h4 id='messageTitle'>Send a message</h4>
+        <form id='messageForm' onSubmit={(e) => handleSubmit(e)}>
+            <textarea id='message' onChange={(event) => setMessage(event.target.value)}/>
+            <div>
+            <input type='checkbox' id='selectAll' />
+            <label>Select All</label>
+            {collabs.map((person) => 
+            <div key={person}>
+                <input className='collaborator' type='checkbox' checked={isChecked} /> 
+                <label>{person}</label>
+            </div>
+            )}
+            </div> 
+            <input type='submit' value='Send'/>
+        </form>
     </div>
   );
 }
