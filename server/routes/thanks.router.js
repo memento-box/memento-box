0b6b529ac7175335c5dc23c65ace556b02162e05
Sarah McCartney <smmc16@gmail.com>
2024-07-17 19:57:43 -0500

    changed /messages GET route

--- server/routes/thanks.router.js
+++ server/routes/thanks.router.js
@@ -6,10 +6,10 @@ router.get('/messages', (req, res) => {
 queryText = `
 SELECT "memento_box"."recipient_name", "box_thanks"."message" FROM "memento_box" 
 JOIN "box_thanks" ON "box_thanks"."box_id" = "memento_box"."id"
-WHERE "box_thanks"."box_id" = 2 AND "box_thanks"."thanks_receiver_id" = 1;
+WHERE "box_thanks"."box_id" = $1;
 `;
 pool
-    .query(queryText, [req.body.boxID, req.body.userID])
+    .query(queryText, [req.body.boxID])
     .then((dbRes) => {res.status(200).send(dbRes.rows)})
     .catch((err) => {
       console.log('Fetching thanks messages failed:', err);
