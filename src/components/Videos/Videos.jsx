9f79a3d3629799987d96f0e3bb324136996ebf74
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-07-17 20:07:43 -0500

    Changed how data processes, on file change the video will now upload to cloudinary, added button to send to box (send to server).

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -12,20 +12,20 @@ const Videos = () => {
     const [fileUpload, setFileUpload] = useState([]);
     const [fileMap, setFileMap] = useState([])
 
-    const videoUpload = () => { //Uploads video to cloudinary and returns media url
-
-      console.log(fileUpload);
+    const videoUpload = async (e) => { //Uploads video to cloudinary and returns media url
+      
+      const uploadCloud = e.target.files[0];
 
       const formData = new FormData();
-      formData.append('file', fileUpload);
+      formData.append('file', uploadCloud);
       formData.append('upload_preset', uploadPreset);
       let apiUrl = `https://api.cloudinary.com/v1_1/${cloudName}/video/upload`; //appending cloud names & presets to url
 
       axios.post(apiUrl, formData)
       .then((r) => {  //creating an array for the video urls
         console.log(r.data);
-        const videoUrl = r.data
-        setFileMap([...fileMap, videoUrl])
+        const videoData = r.data
+        setFileMap([...fileMap, videoData])
       })
       .catch((e) => {
         console.log("Something went wrong with your video upload", e)
@@ -33,7 +33,12 @@ const Videos = () => {
 
     }
 
+    const serverUpload =(video) => {
+        console.log(video);
+    }
+
     const deleteVideo = (video) => { //TERNERARY OPERATOR FOR DELETE TOKEN IF IT EXISTS
+        
         const formData = new FormData();
         formData.append('token', video.delete_token)
         let destroyUrl = `https://api.cloudinary.com/v1_1/${cloudName}/delete_by_token` //deletes using upload token within 10 min
@@ -59,8 +64,8 @@ const Videos = () => {
            Video Upload: 
 
         {/*Form to upload videos to Cloudinary*/}
-        <form onSubmit={videoUpload}>
-            <input type='file' accept='video/*' onChange={(e) => setFileUpload(e.target.files[0])}/>
+        <form>
+            <input type='file' accept='video/*' onChange={videoUpload}/>
             <button type='submit'>Upload Video</button>
         </form>
 
@@ -71,6 +76,7 @@ const Videos = () => {
                     return <>
                     <ReactPlayer url={file.url} controls />
                     <button onClick={() => deleteVideo(file)}>Delete Video</button>
+                    <button onClick={() => serverUpload(file)}>Add Video To Box</button>
                     </>
                 })
             ) : (<p>No Videos To Display</p>)
