d61ef1ac6b848c6e395185d07a3eb499c34de215
Sean Harrison <sharrison9@gmail.com>
2024-07-22 02:37:54 -0500

    Update footer and recipient box styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './Footer.css';
 
-
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
 function Footer() {
-  
   const url = 'https://us14.list-manage.com/contact-form?u=b443c9da3e589e0f1325e7f34&form_id=778634d15230b49196fc729fbc061075';
+
+  const handleClick = () => {
+    window.open(url, '_blank');
+  };
+
   return (
-  <footer>
-    <p onClick={() => window.open(url)}>Contact Us</p>
-  </footer>
-  
-  )
+    <footer>
+      <p onClick={handleClick}>Contact Us</p>
+    </footer>
+  );
 }
 
 export default Footer;
