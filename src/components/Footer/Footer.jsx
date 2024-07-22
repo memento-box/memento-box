2cc7c558895af226cba3bcfa0c8ceb16b3f1bcea
Mohamed A Ali <148814066+Mohagosaar@users.noreply.github.com>
2024-07-22 08:07:18 -0500

    added new footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-  const url = 'https://us14.list-manage.com/contact-form?u=b443c9da3e589e0f1325e7f34&form_id=778634d15230b49196fc729fbc061075';
+  const url =
+    "https://us14.list-manage.com/contact-form?u=b443c9da3e589e0f1325e7f34&form_id=778634d15230b49196fc729fbc061075";
 
   const handleClick = () => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
   return (
-    <footer>
+    <footer className="">
       <p onClick={handleClick}>Contact Us</p>
     </footer>
   );
