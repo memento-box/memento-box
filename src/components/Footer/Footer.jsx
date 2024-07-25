8e058e48f585ce0e347b5ce224885183561e5912
Kenneth Grina <grina.kenneth@gmail.com>
2024-07-24 20:32:23 -0500

    readme updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 
 function Footer() {
   const url =
-    "https://us14.list-manage.com/contact-form?u=b443c9da3e589e0f1325e7f34&form_id=778634d15230b49196fc729fbc061075";
+    "ADD MAILCHIMP FORM URL HERE";
 
   const handleContactClick = (e) => {
     e.preventDefault();
